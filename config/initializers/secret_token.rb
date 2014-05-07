0437627e524969535343a580e5bf6d7785823ef0
Nick Dubus <nico.dubus17@gmail.com>
2014-05-07 12:31:32 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pumpgrade::Application.config.secret_key_base = '53677d3557ef95ee7189e3a22280d63998942d0cefb1b2e6f98443cbe014d24812302306b422a8094c885a4bde3384853ba4901eb48998b2f6f09d7595d21301'
