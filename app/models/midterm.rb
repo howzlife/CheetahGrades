257a2ee106e602de02a69cba8737c9578ff0b5b4
Nick Dubus <nico.dubus17@gmail.com>
2014-05-13 11:59:57 -0400

    Added unit tests for midterm models

--- app/models/midterm.rb
+++ app/models/midterm.rb
@@ -1,4 +1,5 @@
 class Midterm < ActiveRecord::Base
   validates :school, :courseCode, :year, :professor, :semester, presence: true
+  validates :year, :numericality => { :greater_than_or_equal_to => 1990, :less_than_or_equal_to => 2014 }
  
 end
--- test/models/midterm_test.rb
+++ test/models/midterm_test.rb
@@ -1,7 +1,42 @@
 require 'test_helper'
 
 class MidtermTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    test "midterm attributes must not be empty" do
+    midterm = Midterm.new
+    assert midterm.invalid?
+    assert midterm.errors[:school].any?
+    assert midterm.errors[:courseCode].any?
+    assert midterm.errors[:year].any?
+    assert midterm.errors[:professor].any?
+    assert midterm.errors[:semester].any?
+  end
+  
+    test "midterm year must be greater than 1990 and less than 2014" do
+    midterm = Midterm.new(school:      "University of Ottawa",
+                          courseCode:  "xyz",
+                          professor:   "McGonnigle",
+                          semester:    "Fall")
+    midterm.year = -1
+    assert midterm.invalid?
+    assert_equal ["year must be between 1990 and 2014"]
+      midterm.errors[:year]
+
+    midterm.year = 0
+    assert midterm.invalid?
+    assert_equal ["year must be between 1990 and 2014"], 
+      midterm.errors[:year]
+      
+    midterm.year = 1989
+    assert midterm.invalid?
+    assert_equal ["year must be between 1990 and 2014"], 
+     midterm.errors[:year]
+     
+    midterm.year = 2014
+    assert midterm.invalid?
+    assert_equal ["year must be between 1990 and 2014"], 
+      midterm.errors[:year]
+
+    midterm.year = 2000
+    assert midterm.valid?
+  end
 end
